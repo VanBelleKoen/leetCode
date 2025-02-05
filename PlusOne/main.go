74ad67b06b8b568732702065cfc12ee08375fad6
KoenVanBelle <koen.vanbelle@bignited.be>
2025-02-05 14:34:17 +0100

    Feat: Solve plusOne challenge

---
+++ PlusOne/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func plusOne(digits []int) []int {
+	for i := len(digits) - 1; i >= 0; i-- {
+		if digits[i] < 9 {
+			digits[i]++
+			return digits
+		}
+		digits[i] = 0
+	}
+	return append([]int{1}, digits...)
+}
+
+func main() {
+	digits := []int{1, 2, 3}
+	result := plusOne(digits)
+	fmt.Println(result)
+}
